08da43831ae900950f1a42f9280447ba6be24b67
guyufeng <32313941@qq.com>
2022-03-08 17:56:46 +0800

    3.8

--- pages/home-music/index.js
+++ pages/home-music/index.js
@@ -1,75 +1,71 @@
-import getBanners from '../../service/musicData'
+import {getBanners} from '../../service/musicData'
+import queryRect from '../../utils/query-rect'
+import {throttle} from '../../utils/throttle'
+// throttle返回一个真正执行的节流函数, 传入要节流的方法queryRect
+const queryThrottle = throttle(queryRect, 1000)
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    bannersList: [],
+    swiperHeight: 0  //swiper容器高度,动态计算适配小屏
   },
   goSearch() {
     wx.navigateTo({
       url: '/pages/home-search/index',
     })
   },
+  // 图片加载完成的回调函数
+  imageLoaded() {
+    // imageLoaded回调方法，会计算所有的banners图片，通过节流函数优化只在单位时间内计算一张图片
+    // 1.调用新节流函数，2.queryRect返回一个promise, 通过then回调获取高度
+    queryThrottle('.swiper-image').then(res => {
+      console.log('图片属性===》', res) // 只执行了1次
+      this.setData({
+        swiperHeight: res[0].height
+      })
+    })
+  },
   // 获取页面数据
   getPageData() {
     getBanners().then(res => {
       console.log('轮播图====', res)
+      this.setData({
+        bannersList: res.banners
+      })
     })
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
+
   onLoad: function (options) {
     this.getPageData()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
   onHide: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload: function () {
 
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh: function () {
 
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
 
   },
-
-  /**
-   * 用户点击右上角分享
-   */
   onShareAppMessage: function () {
 
   }
--- utils/query-rect.js
+++ utils/query-rect.js
@@ -1,12 +1,13 @@
 export default function (selector) {
+  // 返回promise对象，供外部回调
   return new Promise((resolve) => {
     const query = wx.createSelectorQuery()
     // 获取要查询矩形元素的样式信息
-    query.select(slector).boundingClientRect()
+    query.select(selector).boundingClientRect()
     query.exec((res) => {
       resolve(res)
     })
-    // 增强写法
-    // query.exec(resolve)
+    // 增强写法 少执行一个函数，提高性能
+    // query.exec(resolve) 
   })
 }
